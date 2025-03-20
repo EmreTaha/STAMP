bfb35f54a150ba207c3448b0daa1dcc35a05f378
EmreTaha <emretaha@gmail.com>
2025-03-20 18:47:02 +0100

    readme

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,10 +1,6 @@
 import random
 import numpy as np
 import torch
-import torch.nn as nn
-import io
-import requests
-import monai
 
 
 def initialize(seed=3407, allow_tf32=False, deterministic=True):
@@ -22,14 +18,3 @@ def initialize(seed=3407, allow_tf32=False, deterministic=True):
     torch.backends.cuda.matmul.allow_tf32 = allow_tf32
 
     np.random.seed(seed)
-
-def worker_init_fn(worker_id):                                                          
-    np.random.seed(torch.initial_seed() // 2**32 + worker_id)
-    random.seed(torch.initial_seed() // 2**32 + worker_id)
-
-
-def get_BiT_weights(bit_variant='BiT-M-R50x1-CIFAR10'):
-    #TODO this doesnt belong here!
-    response = requests.get(f'https://storage.googleapis.com/bit_models/{bit_variant}.npz')
-    response.raise_for_status()
-    return np.load(io.BytesIO(response.content))
--- utils/model_helpers.py
+++
@@ -1,6 +0,0 @@
-def convert_model_to_fp32(model):
-    # Converts a mixed-precision model to fp32 for evaluation
-    for p in model.parameters():
-        p.data = p.data.float()
-        if p.grad:
-            p.grad.data = p.grad.data.float()
